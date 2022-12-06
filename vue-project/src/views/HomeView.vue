de7b2266ff11dba5f25865ba4429514aa2ce9d30
Anton Zhiliakov <gofranich@gmail.com>
2022-12-06 16:58:29 +0600

    Redirect route

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -26,6 +26,7 @@
               v-for="searchResult in mapboxSearchResults"
               :key="searchResult.id"
               class="py-2 cursor-pointer"
+              @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -38,6 +39,23 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import {useRouter} from "vue-router";
+
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  console.log(searchResult)
+  const[city, state] = searchResult.place_name.split(",")
+  router.push({
+    name: "cityView",
+    params: {state: state.replace(" ", ""), city: city},
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  });
+}
 const mapboxAPIKey =
     "pk.eyJ1Ijoia290ZWwiLCJhIjoiY2xiNmJ4OG1sMDJzbjNuc3hxbnM5dHhneiJ9.SQ6vmHcXR_h2pI79c87Ksg";
 const searchQuery = ref("");
